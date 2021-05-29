34dd00aa52beda39e0d3b5b9e4d22d34b9bd9ed8
MADHUSUDHAN <madhusudhan002.mr@gmail.com>
2021-05-29 07:07:13 +0530

    added a comment to 01.print.sh file

--- 01-print.sh
+++ 01-print.sh
@@ -14,7 +14,8 @@ echo -e "\nhai,\n\nwelcome to the world"
 
 echo -e "A phrase is a group of words that express a concept and
              is used as a unit within a sentence. Eight common types of
-             phrases are: noun, verb, gerund, infinitive, appositive, participial,
-             prepositional, and absolute. Take a look at our selection of phrase
-             examples below."
+             phrases are: noun, verb, gerund, infinitive, appositive,
+             participial,prepositional, and absolute. Take a look at our
+             selection of phrase examples below."
+
 
