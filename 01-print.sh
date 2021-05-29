abe749361630fc01187a87b3d6360c8a76a9ac84
MADHUSUDHAN <madhusudhan002.mr@gmail.com>
2021-05-29 07:06:17 +0530

    added a comment to 01.print.sh file

--- 01-print.sh
+++ 01-print.sh
@@ -11,3 +11,10 @@ echo hai im madhu
 # syntax : echo -e "line1\nline2"
 
 echo -e "\nhai,\n\nwelcome to the world"
+
+echo -e "A phrase is a group of words that express a concept and
+             is used as a unit within a sentence. Eight common types of
+             phrases are: noun, verb, gerund, infinitive, appositive, participial,
+             prepositional, and absolute. Take a look at our selection of phrase
+             examples below."
+
