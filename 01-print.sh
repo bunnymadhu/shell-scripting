73cc09811a72cd6a06b90507e46297966698734a
MADHUSUDHAN <madhusudhan002.mr@gmail.com>
2021-05-29 06:45:07 +0530

    added a comment to 01.print.sh file

---
+++ 01-print.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# echo and printf commands can be use to print some message on the screen
+# however echo command we use here,because it is simple and yet effective to all the needs.
+
+# syntax : echo input message
+
+echo hai im madhu
