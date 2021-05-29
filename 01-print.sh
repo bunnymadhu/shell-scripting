b23eec32b9edf1c63dfe9ce8b1a3cc48a92ff2a3
MADHUSUDHAN <madhusudhan002.mr@gmail.com>
2021-05-29 07:39:14 +0530

    added a comment to 01.print.sh file

--- 01-print.sh
+++ 01-print.sh
@@ -34,3 +34,6 @@ echo -e "\t A phrase is a group of words that express a concept and
 echo -e "\e[36mGoodmorning"
 echo -e "\e[33mhai to evryone\e[31mhello"
 echo -e "\e[32mthis is madhu\e[35mbtch complted"
+
+#echo text in normol colour
+# syntax : echo -e "\e[31msathya is cool\e[0m"
