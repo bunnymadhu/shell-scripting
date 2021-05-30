7a9aea3b424f7b7e9e54a00ce5fe9d80cfa1615f
MADHUSUDHAN <madhusudhan002.mr@gmail.com>
2021-05-30 07:11:30 +0530

    added a comment to 03.varible.sh file

--- 03-variable.sh
+++ 03-variable.sh
@@ -9,3 +9,10 @@ echo Course Name = $COURSE
 DATE =2021-05-30
 DATE = $(date +%F)
 echo Good Morning, Today date is $DATE
+
+NO_OF_USERS = $(who | wc -1)
+echo number of users logged in system = $NO_OF_USERS
+
+NO_OF_LINES IN etc/passwd = $(wc /etc/passwd)
+echo number of lines in etc/passwd = $NO_OF_LINES
+
