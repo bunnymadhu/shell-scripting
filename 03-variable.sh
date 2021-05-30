d60955cbc64ed9ac670a8061394a14c12c7b4e89
MADHUSUDHAN <madhusudhan002.mr@gmail.com>
2021-05-30 07:16:26 +0530

    added a comment to 03.varible.sh file

--- 03-variable.sh
+++ 03-variable.sh
@@ -16,3 +16,6 @@ echo number of users logged in system = $NO_OF_USERS
 NO_OF_LINES IN etc/passwd = $(wc /etc/passwd)
 echo number of lines in etc/passwd = $NO_OF_LINES
 
+ADD = $((1+2+3))
+BIG = $((4+5-3/6x80/5-2x900/8))
+echo BIG = $BIG
