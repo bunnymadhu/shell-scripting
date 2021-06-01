b3a1f799edd7ffa236826cd68c24334e31aee4a7
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-01 19:07:57 +0530

    add command in 04.input.sh file

--- 05-inputs.sh
+++ 05-inputs.sh
@@ -3,19 +3,17 @@
 #to print or get the script name
 echo $0
 
-#to print first line  $1
-#to print second line  $2
-#like wise u can go to the ...$n
-
+#to print first line
 echo $1
 
+#to print second line
 echo $2
 
-#to print all arguments $* or $@
+#like wise u can go to the ...$n
 
+#to print or get all arguments parsed
 echo $*
-
 echo $@
 
-#to print number of arguments $#
+#to print number of arguments
 echo $#
