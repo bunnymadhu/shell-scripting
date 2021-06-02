ba0e47789309c639e0082328147dc33ff4e22077
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-02 22:45:42 +0530

    some changes done in the  makefile for roboshop

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -10,6 +10,6 @@ echo -e  "\e[1m-----------------------------------------------------------------
 echo  -e "\e[1;32mSUCCESS"
 echo  -e "\e[1m---------------------------------------------------------------------\e[0m"
 
-echo "installinh Nginx ..."
+echo -n "installing Nginx ..."
 yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[32mdone\e[0m"
