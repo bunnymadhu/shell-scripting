040c265e36116836bd9d8d1780c62a6ee48f4701
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 10:57:55 +0530

    make some changes  in common.sh file

--- roboshop/Components/common.sh
+++ roboshop/Components/common.sh
@@ -6,6 +6,7 @@ STAT () {
   if [ $1 -eq 0 ]
   then
     echo -e "\e[1;32m done\e[0m"
+
   else
     echo -e "\e[1;31m fail\e[0m"
     echo -e "\e[1;33m check the log file more detail ...log file : /tmp/roboshop.log "
