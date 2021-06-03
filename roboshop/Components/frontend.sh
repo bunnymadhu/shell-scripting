34cfcb354f9b9dab851c2e34e2e5c295946eabc2
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 13:07:08 +0530

    make some changes  in frontend.sh file

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -3,17 +3,29 @@
 source Components/common.sh
 rm -f /tmp/roboshop.log
 
-HEAD "Installing Nginx"
+HEAD "Installing Nginx\t"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Start Nginx\t"
+HEAD "Start Nginx\t\t"
 systemctl start nginx &>>/tmp/roboshop.log
 systemctl enable nginx &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "download from Github"
+HEAD "download from Github\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Delete old HTML DOCS\t"
+ rm -rf  /usr/share/nginx/html/*
+ STAT $?
+
+ HEAD "Extract Downloaded Content"
+ unzip -d /usr/share/nginx/html  /tmp/frontend.zip &>>/tmp/roboshop.log
+ STAT $?
+
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
