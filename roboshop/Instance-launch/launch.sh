91cbba0f04980d6460584d19c31efc8e1c5cbc38
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 19:07:08 +0530

    make some changes  in instance_launch

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -4,8 +4,8 @@ COMPONENT = $1
 
 ##  -z validates the variable empty, true if it is empty.
 
-if [ -z "${COMPONENT}" ]; then
- echo "COMPONENT input  is needed"
+if [ -z "${COMPONENT}"  ]; then
+ echo "Component input  is needed"
  exit 1
 fi
 
