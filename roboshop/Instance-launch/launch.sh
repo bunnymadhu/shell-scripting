12ee7d47e289a835a069637df456b9881f106d84
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 19:12:47 +0530

    make some changes  in instance_launch

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -12,4 +12,6 @@ fi
 LAUNCHTEMPLATEID=lt-0132406f31a96f585
 LAUNCHTEMPLATEVERSION=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=name,Value=${COMPONENT}}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=name,Value=${COMPONENT}}]" | jq
+
+## | jq: to avoid that everytime instead press= q
