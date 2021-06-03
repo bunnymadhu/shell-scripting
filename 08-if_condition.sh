d563ad1943b0742e869ad4908ea96aff72411968
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 11:33:54 +0530

    make some changes  in if_condition.sh file

---
+++ 08-if_condition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#string comparison
+
+read -p "Enter Username: " username
+
+if [ "$username" == "root" ]; then
+  echo "Hey,User $username is a Admin User"
+else
+  echo "Hey, User $username is a Normal User"
+fi
