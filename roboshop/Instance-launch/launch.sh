9d27b9cb295fa134783bb8a215a7c069f534fb88
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 20:11:12 +0530

    make some changes  in instance_launch

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -20,6 +20,11 @@ if [ "${INSTANCE_STATE}" = "running" ]; then
   exit 0
 fi
 
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "Instances already exists,not createing any !!"
+  exit 0
+fi
+
 aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=name,Value=${COMPONENT}}]" | jq
 
 ## | jq: to avoid that everytime instead press= q
