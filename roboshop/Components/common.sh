1acdbbc4e8f7aa60b438256b632a85aa48435308
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-03 09:41:09 +0530

    make some changes  in common.sh file

---
+++ roboshop/Components/common.sh
@@ -0,0 +1,8 @@
+HEAD () {
+  if -n "\e[1;32m done\e[0m"
+  echo -e "my script is success"
+}
+
+STAT () {
+
+  }
--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo -e  "\e[1m---------------------------------------------------------------------"
-echo  -e "installing Nginx"
-echo  -e "---------------------------------------------------------------------\e[0m"
-
-yum install nginx -y
-
-echo -e  "\e[1m---------------------------------------------------------------------"
-echo  -e "\e[1;32mSUCCESS"
-echo  -e "\e[1m---------------------------------------------------------------------\e[0m"
-
-echo -n "installing Nginx ..."
+HEAD -n "installing Nginx ..."
 yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[32mdone\e[0m"
