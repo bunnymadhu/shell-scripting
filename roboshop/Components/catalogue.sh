f3ab9ba6ecab80c050b163b3358d1e99f6892c5f
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-05 11:58:58 +0530

    make some changes  in catalogue.sh

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -9,8 +9,14 @@ yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
 HEAD "Add Roboshop  APP  User\t"
-useradd roboshop &>>/tmp/roboshop.log
-STAT $?
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo User is already there,so avoid the User creation &>>/tmp/roboshop.log
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi
+
 
 HEAD "Download from Github\t\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
