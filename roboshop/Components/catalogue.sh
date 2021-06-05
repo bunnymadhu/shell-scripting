0f2d64dac18609c2a20569fd850a197a38243a6e
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-05 11:34:05 +0530

    make some changes  in catalogue.sh

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -4,15 +4,15 @@ source Components/common.sh
 rm -f /tmp/roboshop.log
 disable-auto-shutdown
 
-HEAD "Insatlling Nodejs"
+HEAD "Insatlling Nodejs\t\t"
 yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Add Roboshop  APP  User "
+HEAD "Add Roboshop  APP  User\t"
 useradd roboshop &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Download from Github"
+HEAD "Download from Github\t\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
@@ -20,7 +20,7 @@ HEAD "Extract Downloaded Content\t"
 cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
 STAT $?
 
-HEAD "Install Nodejs Dependencies"
+HEAD "Install Nodejs Dependencies\t"
 cd /home/roboshop/catalogue &&  npm install &>>/tmp/roboshop.log
 STAT $?
 
