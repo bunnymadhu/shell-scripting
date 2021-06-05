563ad13baa6fc244f14a9e99caa8efe5c5ca2834
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-05 12:45:28 +0530

    make some changes  in catalogue.sh

--- roboshop/Components/catalogue.sh
+++ roboshop/Components/catalogue.sh
@@ -23,7 +23,7 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
 STAT $?
 
 HEAD "Extract Downloaded Content\t"
-rm -rf catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+cd /home/roboshop && rm -rf catalogue &&  unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
 STAT $?
 
 HEAD "Install Nodejs Dependencies\t"
