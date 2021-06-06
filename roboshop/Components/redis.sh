b3009c782bae6f9da0a0e51143d75191426ae112
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-06 09:16:50 +0530

    make some changes  in redis.sh

--- roboshop/Components/redis.sh
+++ roboshop/Components/redis.sh
@@ -9,11 +9,11 @@ HEAD "Setup Redis Repos\t\t\t"
 yum install epel-release yum-utils  http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Install Redis\t\t\t"
+HEAD "Install Redis\t\t\t\t"
 yum install redis -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Update the address in Redis.config"
+HEAD "Update the address in Redis.config\t"
 sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf
 STAT $?
 
