2632277b701062d37486e990ad7769d6ecef2116
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-06 18:15:34 +0530

    make some changes  in mysql.sh

--- roboshop/Components/mysql.sh
+++ roboshop/Components/mysql.sh
@@ -5,7 +5,7 @@ rm -f /tmp/roboshop.log
 set-hostname mysql
 disable-auto-shutdown
 
-HEAD "Setup MySQL Repos\t\t"
+HEAD "Setup MySQL Repos\t"
 echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
@@ -17,6 +17,6 @@ HEAD "Installing of MySQL Server"
 yum remove mariadb-libs -y &>>/tmp/roboshop.log && yum install mysql-community-server -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Start MySQL Services\t\t"
+HEAD "Start MySQL Services\t"
 systemctl enable mysqld &>>/tmp/roboshop.log && systemctl start mysqld &>>/tmp/roboshop.log
 STAT $?
