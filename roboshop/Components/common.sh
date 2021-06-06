8c1f21aa0b626a42949ba187276148ada0a02a6e
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-06 11:46:39 +0530

    make some changes  in common.sh

--- roboshop/Components/common.sh
+++ roboshop/Components/common.sh
@@ -13,3 +13,54 @@ STAT () {
   fi
   }
 
+NODEJS () {
+  HEAD "Installing Nodejs\t\t\t"
+  yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+  STAT $?
+
+  HEAD "Add Roboshop  APP  User\t\t"
+  id roboshop &>>/tmp/roboshop.log
+  if [  $? -eq 0 ]; then
+    echo User is already there, so avoid the User creation &>>/tmp/roboshop.log
+    STAT $?
+  else
+    useradd roboshop &>>/tmp/roboshop.log
+    STAT $?
+  fi
+
+  HEAD "Download from Github\t\t\t"
+  curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+  STAT $?
+
+  HEAD "Extract Downloaded Content\t\t"
+  cd /home/roboshop && rm -rf catalogue &>>/tmp/roboshop.log
+  STAT $?
+
+  ## in root user cd /home/roboshop/
+  ## ls ------ catalogue-main
+  ## mv catalogue-main catalogue,,,the go to centos user
+
+  HEAD "unzip the file\t\t\t\t"
+  unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+  STAT $?
+
+  HEAD "Install Nodejs Dependencies\t\t"
+  cd /home/roboshop/catalogue &&  npm install --unsafe-perm &>>/tmp/roboshop.log
+  STAT $?
+
+  HEAD "Fix the permissions to the App Content"
+  chown roboshop:roboshop /home/roboshop -R
+  STAT $?
+
+  HEAD "Update DNS Records in systemD file\t"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+  STAT $?
+
+  HEAD "Setup SystemD service\t\t\t"
+  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+  STAT $?
+
+  HEAD "Start Catalogue service\t\t"
+  systemctl daemon-reload && systemctl enable catalogue &>>/tmp/roboshop.log && systemctl restart catalogue &>>/tmp/roboshop.log
+  STAT $?
+}
