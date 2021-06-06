50d2f74dc0f0db5403b9c327e1ab5ae468014c5f
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-06 08:23:46 +0530

    make some changes  in launch.sh

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -13,22 +13,28 @@ LAUNCHTEMPLATEVERSION=1
 
 ##  validate if instance is running r not
 
+DNS_UPDATE () {
+  PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
+  sed  -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z03444518JCZ2U6FF5S6 --change-batch file:///tmp/record.json | jq
+}
+
 INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].State.Name | xargs -n1)
 if [ "${INSTANCE_STATE}" = "running" ]; then
   echo "Instances already exists,not creating any !!"
+  DNS_UPDATE
   exit 0
 fi
 
 if [ "${INSTANCE_STATE}" = "stopped" ]; then
   echo "Instances already exists,not creating any !!"
+  DNS_UPDATE
   exit 0
 fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 sleep 30
-PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
-sed  -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z03444518JCZ2U6FF5S6 --change-batch file:///tmp/record.json | jq
+DNS_UPDATE
 
 
 
