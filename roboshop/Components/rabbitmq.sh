dbf2eedad5fa74c02bdcf4b0d30e8364ac88bf22
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-07 23:03:19 +0530

    make some changes  in rabbitmq.sh

--- roboshop/Components/rabbitmq.sh
+++ roboshop/Components/rabbitmq.sh
@@ -6,9 +6,14 @@ set-hostname rabbitmq
 disable-auto-shutdown
 
 HEAD "Install ERLNG\t\t\t"
-yum list installed | grep erlang
+yum list installed | grep erlang  &>>/tmp/roboshop.log
+if [  $? -eq 0 ]; then
+  echo "ERLNG already installed" &>>/tmp/roboshop.log
+  STAT $?
+else
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>/tmp/roboshop.log
 STAT $?
+fi
 
 HEAD "Setup Rabbitmq Yum Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>>/tmp/roboshop.log
