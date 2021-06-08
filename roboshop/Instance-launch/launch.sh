19b8c4fb1de98e253475381ee199beff9f2c1497
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-08 21:38:09 +0530

    make some changes  in common.sh

--- roboshop/Instance-launch/launch.sh
+++ roboshop/Instance-launch/launch.sh
@@ -13,6 +13,12 @@ LAUNCHTEMPLATEVERSION=2
 
 ##  validate if instance is running r not
 
+if  [ "${COMPONENT}" == "all" ]; then
+  for Component in mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+
+  done
+fi
+
 DNS_UPDATE () {
   PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
   sed  -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
@@ -22,22 +28,34 @@ DNS_UPDATE () {
 ## here am searching for COMPONENT AND REPLACING WITH THE VARIABLE OF ${COMPONENT} of json file
 ## here am searching for IPADDRESS AND REPLACING WITH THE VARIABLE OF ${PRIVATEIP} of json file
 
-INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].State.Name | xargs -n1)
-if [ "${INSTANCE_STATE}" = "running" ]; then
-  echo "Instances already exists,not creating any !!"
+INSTANCE_CREATE() {
+  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  if [ "${INSTANCE_STATE}" = "running" ]; then
+    echo "${COMPONENT} Instances already exists,not creating any !!"
+    DNS_UPDATE
+    return 0
+  fi
+
+  if [ "${INSTANCE_STATE}" = "stopped" ]; then
+    echo "${COMPONENT} Instances already exists,not creating any !!"
+    DNS_UPDATE
+    return 0
+  fi
+
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  sleep 10
   DNS_UPDATE
-  exit 0
+}
+
+if  [ "${COMPONENT}" == "all" ]; then
+  for component in mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+  COMPONENT=$component
+  INSTANCE_CREATE
+  done
+else
+  INSTANCE_CREATE
 fi
 
-if [ "${INSTANCE_STATE}" = "stopped" ]; then
-  echo "Instances already exists,not creating any !!"
-  DNS_UPDATE
-  exit 0
-fi
-
-aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
-sleep 10
-DNS_UPDATE
 
 
 
