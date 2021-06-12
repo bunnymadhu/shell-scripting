259464b14444563d2b5feea198061e92e0028f56
bunnymadhu <84825120+bunnymadhu@users.noreply.github.com>
2021-06-12 22:24:57 +0530

    make some changes  in launch.sh

--- ansible/Instance-launch/launch.sh
+++ ansible/Instance-launch/launch.sh
@@ -20,7 +20,7 @@ LAUNCHTEMPLATEVERSION=2
   ##aws route53 change-resource-record-sets --hosted-zone-id Z03444518JCZ2U6FF5S6 --change-batch file:///tmp/record.json | jq
 ##}
 DNS_UPDATE() {
-  PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
+  PRIVATEIP=$(aws --region us-east-1  ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
   sed -e "s/COMPONENT/$COMPONENT/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z03444518JCZ2U6FF5S6 --change-batch file:///tmp/record.json | jq
 }
@@ -29,7 +29,7 @@ DNS_UPDATE() {
 ## here am searching for IPADDRESS AND REPLACING WITH THE VARIABLE OF ${PRIVATEIP} of json file
 
 INSTANCE_CREATE() {
-  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT} "  | jq .Reservations[].Instances[].State.Name | xargs -n1)
   if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "${COMPONENT} Instances already exists,not creating any !!"
     DNS_UPDATE
@@ -43,7 +43,7 @@ INSTANCE_CREATE() {
   fi
 
    echo -n Instance ${COMPONENT} created - IPADDRESS is
-  aws ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress | xargs -n1
+  aws --region us-east-1 ec2 run-instances --launch-template LaunchTemplateId=${LAUNCHTEMPLATEID},Version=${LAUNCHTEMPLATEVERSION} --tag-specifications "ResourceType=instance ,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress | xargs -n1
   sleep 10
   DNS_UPDATE
 }
